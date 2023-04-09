b96149b9309ba006ea8effe8c718bb7cd3eb417d
m0slah <muhammedslahqadir@gmail.com>
2023-04-09 10:05:27 +0300

    add Post Component

--- components/Post/index.jsx
+++ components/Post/index.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import cl from 'classnames';
+import Link from 'next/link';
+import Image from 'next/image';
 
-function index() {
+import { urlFor } from '../../lib/client';
+import Title from '../Title';
+
+import styles from './index.module.scss';
+
+const Post = ({
+    className,
+    image,
+    title,
+    description,
+    slug
+}) => {
   return (
-    <div>index</div>
+    <Link
+      href={`/post/${encodeURIComponent(slug.current)}`}
+      className={cl(className, styles.post)}
+    >
+        <a className={styles.postLink}>
+          <Title
+            type="small"
+            className={styles.postTitle}
+          >
+            {title}
+          </Title>
+          <div className={styles.postContent}>
+            <div className={styles.postImage}>
+              <Image
+                src={urlFor(image).url()}
+                alt={image.caption}
+                width="100"
+                height="100"
+              />
+            </div>
+            <p
+              className={styles.postDescription}
+            >
+              {description}
+            </p>
+          </div>
+        </a>
+    </Link>
   )
 }
 
-export default index
+export default Post;
