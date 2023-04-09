1782c14a0bfefc6c204c80643834e4db021f523e
m0slah <muhammedslahqadir@gmail.com>
2023-04-09 10:00:17 +0300

    add Button component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "../styles/global.scss";
+import "../styles/reset.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
