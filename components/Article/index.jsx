60a08de004305ba3ae682d700019720f38bff940
m0slah <muhammedslahqadir@gmail.com>
2023-04-09 10:01:34 +0300

    add article componet

--- components/Article/index.jsx
+++ components/Article/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import cl from 'classnames';
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
-function index() {
+import styles from './index.module.scss';
+
+const Article = ({
+    children,
+    backUrl,
+    className
+}) => {
   return (
-    <div>index</div>
+    <article className={cl(className, styles.article)}>
+      <Link href={backUrl}>
+        <a className={styles.articleBack}>
+          <AiOutlineArrowLeft />
+        </a>
+      </Link>
+      <div className={styles.articleContent}>
+        {children}
+      </div>
+    </article>
   )
 }
 
-export default index
+export default Article;
