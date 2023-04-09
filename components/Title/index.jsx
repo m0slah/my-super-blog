f6aaa7ad8e6ede78048c27883856d262aa076943
m0slah <muhammedslahqadir@gmail.com>
2023-04-09 10:10:11 +0300

    add Title Component

--- components/Title/index.jsx
+++ components/Title/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import cl from 'classnames';
 
-function index() {
+import styles from './index.module.scss';
+
+const Title = ({
+    type,
+    className,
+    children
+}) => {
+  if(type === 'small') {
+    return (
+      <h3 className={cl(className, styles.title, styles.titleSmall)}>
+          {children}
+      </h3>
+    );
+  }
+
   return (
-    <div>index</div>
-  )
+    <h2 className={cl(className, styles.title, styles.titleMedium)}>
+        {children}
+    </h2>
+  );
 }
 
-export default index
+export default Title;
